046f0612f1879bc1f3b92b22bac7f7a5a90cbd25
Ivica Veric <phasecode@live.com>
2018-03-10 19:40:24 -0800

    Trying this approach

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,51 +1,52 @@
-declare const Beacon: {
-    namespace: string;
-    protocol: string;
-    types: ({
-        name: string;
-        type: string;
-        fields: ({
+export declare namespace Definitions {
+    const Beacon: {
+        namespace: string;
+        protocol: string;
+        types: ({
             name: string;
             type: string;
-            default: number;
-        } | {
-            name: string;
-            type: {
-                type: string;
-                values: string;
-            };
-            default: {};
-        } | {
-            name: string;
-            type: string;
-            default?: undefined;
-        } | {
-            name: string;
-            type: string;
-            default: boolean;
-        })[];
-    } | {
-        name: string;
-        type: string;
-        fields: ({
-            name: string;
-            type: string;
-        } | {
-            name: string;
-            type: (string | {
-                type: string;
-                items: string;
-            })[];
-        })[];
-    })[];
-    messages: {
-        ProcessRequest: {
-            request: {
+            fields: ({
                 name: string;
                 type: string;
-            }[];
-            response: string;
+                default: number;
+            } | {
+                name: string;
+                type: {
+                    type: string;
+                    values: string;
+                };
+                default: {};
+            } | {
+                name: string;
+                type: string;
+                default?: undefined;
+            } | {
+                name: string;
+                type: string;
+                default: boolean;
+            })[];
+        } | {
+            name: string;
+            type: string;
+            fields: ({
+                name: string;
+                type: string;
+            } | {
+                name: string;
+                type: (string | {
+                    type: string;
+                    items: string;
+                })[];
+            })[];
+        })[];
+        messages: {
+            ProcessRequest: {
+                request: {
+                    name: string;
+                    type: string;
+                }[];
+                response: string;
+            };
         };
     };
-};
-export { Beacon };
+}
--- dist/index.js
+++ dist/index.js
@@ -1,107 +1,109 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var Beacon = {
-    namespace: "avro",
-    protocol: "Beacon",
-    types: [
-        {
-            name: "LambdaRequest",
-            type: "record",
-            fields: [
-                {
-                    name: "requestId",
-                    type: "string",
-                },
-                {
-                    name: "httpMethod",
-                    type: "string",
-                },
-                {
-                    name: "httpParams",
-                    type: { type: "map", values: "string" },
-                    default: {},
-                },
-                {
-                    name: "httpBody",
-                    type: "string",
-                }
-            ],
+var Definitions;
+(function (Definitions) {
+    Definitions.Beacon = {
+        namespace: "avro",
+        protocol: "Beacon",
+        types: [
+            {
+                name: "LambdaRequest",
+                type: "record",
+                fields: [
+                    {
+                        name: "requestId",
+                        type: "string",
+                    },
+                    {
+                        name: "httpMethod",
+                        type: "string",
+                    },
+                    {
+                        name: "httpParams",
+                        type: { type: "map", values: "string" },
+                        default: {},
+                    },
+                    {
+                        name: "httpBody",
+                        type: "string",
+                    }
+                ],
+            },
+            {
+                name: "LogMessage",
+                type: "record",
+                fields: [
+                    {
+                        name: "message",
+                        type: "string",
+                    },
+                    {
+                        name: "eventTimestamp",
+                        type: "string",
+                    },
+                    {
+                        name: "severity",
+                        type: "string",
+                    }
+                ],
+            },
+            {
+                name: "CallbackData",
+                type: "record",
+                fields: [
+                    {
+                        name: "statusCode",
+                        type: "int",
+                        default: 200,
+                    },
+                    {
+                        name: "headers",
+                        type: { type: "map", values: "string" },
+                        default: {},
+                    },
+                    {
+                        name: "body",
+                        type: "string",
+                    },
+                    {
+                        name: "isBase64",
+                        type: "boolean",
+                        default: false,
+                    }
+                ],
+            },
+            {
+                name: "LambdaResponse",
+                type: "record",
+                fields: [
+                    {
+                        name: "requestId",
+                        type: "string",
+                    },
+                    {
+                        name: "logs",
+                        type: [
+                            "null",
+                            { type: "array", items: "LogMessage" }
+                        ],
+                    },
+                    {
+                        name: "callbackData",
+                        type: "CallbackData",
+                    }
+                ],
+            }
+        ],
+        messages: {
+            ProcessRequest: {
+                request: [
+                    {
+                        name: "lambdaRequest",
+                        type: "LambdaRequest",
+                    }
+                ],
+                response: "LambdaResponse",
+            },
         },
-        {
-            name: "LogMessage",
-            type: "record",
-            fields: [
-                {
-                    name: "message",
-                    type: "string",
-                },
-                {
-                    name: "eventTimestamp",
-                    type: "string",
-                },
-                {
-                    name: "severity",
-                    type: "string",
-                }
-            ],
-        },
-        {
-            name: "CallbackData",
-            type: "record",
-            fields: [
-                {
-                    name: "statusCode",
-                    type: "int",
-                    default: 200,
-                },
-                {
-                    name: "headers",
-                    type: { type: "map", values: "string" },
-                    default: {},
-                },
-                {
-                    name: "body",
-                    type: "string",
-                },
-                {
-                    name: "isBase64",
-                    type: "boolean",
-                    default: false,
-                }
-            ],
-        },
-        {
-            name: "LambdaResponse",
-            type: "record",
-            fields: [
-                {
-                    name: "requestId",
-                    type: "string",
-                },
-                {
-                    name: "logs",
-                    type: [
-                        "null",
-                        { type: "array", items: "LogMessage" }
-                    ],
-                },
-                {
-                    name: "callbackData",
-                    type: "CallbackData",
-                }
-            ],
-        }
-    ],
-    messages: {
-        ProcessRequest: {
-            request: [
-                {
-                    name: "lambdaRequest",
-                    type: "LambdaRequest",
-                }
-            ],
-            response: "LambdaResponse",
-        },
-    },
-};
-exports.Beacon = Beacon;
+    };
+})(Definitions = exports.Definitions || (exports.Definitions = {}));
