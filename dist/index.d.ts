dda5aa47e78c27a431e02fe5d79cefab57c244e6
Ivica Veric <phasecode@live.com>
2018-03-10 17:46:23 -0800

    Commiting Avro Beacon files for deflector.

---
+++ dist/index.d.ts
@@ -0,0 +1,50 @@
+declare const Beacon: {
+    namespace: string;
+    protocol: string;
+    types: ({
+        name: string;
+        type: string;
+        fields: ({
+            name: string;
+            type: string;
+            default: number;
+        } | {
+            name: string;
+            type: {
+                type: string;
+                values: string;
+            };
+            default: {};
+        } | {
+            name: string;
+            type: string;
+            default?: undefined;
+        } | {
+            name: string;
+            type: string;
+            default: boolean;
+        })[];
+    } | {
+        name: string;
+        type: string;
+        fields: ({
+            name: string;
+            type: string;
+        } | {
+            name: string;
+            type: (string | {
+                type: string;
+                items: string;
+            })[];
+        })[];
+    })[];
+    messages: {
+        ProcessRequest: {
+            request: {
+                name: string;
+                type: string;
+            }[];
+            response: string;
+        };
+    };
+};
---
+++ dist/index.js
@@ -0,0 +1,108 @@
+"use strict";
+var Beacon = {
+    namespace: "avro",
+    protocol: "Beacon",
+    types: [
+        {
+            name: "LambdaRequest",
+            type: "record",
+            fields: [
+                {
+                    name: "requestId",
+                    type: "string",
+                },
+                {
+                    name: "httpMethod",
+                    type: "string",
+                },
+                {
+                    name: "httpParams",
+                    type: { type: "map", values: "string" },
+                    default: {},
+                },
+                {
+                    name: "httpBody",
+                    type: "string",
+                }
+            ],
+        },
+        {
+            name: "LogMessage",
+            type: "record",
+            fields: [
+                {
+                    name: "message",
+                    type: "string",
+                },
+                {
+                    name: "eventTimestamp",
+                    type: "string",
+                },
+                {
+                    name: "severity",
+                    type: "string",
+                }
+            ],
+        },
+        {
+            name: "CallbackData",
+            type: "record",
+            fields: [
+                {
+                    name: "statusCode",
+                    type: "int",
+                    default: 200,
+                },
+                {
+                    name: "headers",
+                    type: { type: "map", values: "string" },
+                    default: {},
+                },
+                {
+                    name: "body",
+                    type: "string",
+                },
+                {
+                    name: "isBase64",
+                    type: "boolean",
+                    default: false,
+                }
+            ],
+        },
+        {
+            name: "LambdaResponse",
+            type: "record",
+            fields: [
+                {
+                    name: "requestId",
+                    type: "string",
+                },
+                {
+                    name: "logs",
+                    type: [
+                        "null",
+                        { type: "array", items: "LogMessage" }
+                    ],
+                },
+                {
+                    name: "callbackData",
+                    type: "CallbackData",
+                }
+            ],
+        }
+    ],
+    messages: {
+        ProcessRequest: {
+            request: [
+                {
+                    name: "lambdaRequest",
+                    type: "LambdaRequest",
+                }
+            ],
+            response: "LambdaResponse",
+        },
+    },
+};
+module.exports = {
+    'Beacon': Beacon
+};
